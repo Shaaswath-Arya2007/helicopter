704b97283d6f878c392e502d8e8328392734e766
Shaaswath-Arya2007 <88329825+Shaaswath-Arya2007@users.noreply.github.com>
2021-09-07 20:33:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,13 @@ function keyPressed() {
 	  }
 	  
 	if (keyCode === DOWN_ARROW) {
-	   //make the static property of packageBody as false
+	  Matter.Body.setStatic(packageBody, false)
+		
+		
+		
+		
+		
+		
 
 	}
 }
